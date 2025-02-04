b0a570170308f5921b277f6e968e2336fc237632
«nastassiamak» <redfox.asya@gmail.com>
2025-02-04 21:16:56 +0300

    add vercel.json

---
+++ dist/src/app.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const setting_1 = require("./setting");
+const blogsRouter_1 = require("./features/blogs/blogsRouter");
+const testing_1 = require("./features/testing");
+const postsRouter_1 = require("./features/posts/postsRouter");
+exports.app = (0, express_1.default)(); // создать приложение
+exports.app.use(express_1.default.json()); // создание свойств-объектов body и query во всех реквестах
+exports.app.use((0, cors_1.default)()); // разрешить любым фронтам делать запросы на наш бэк
+exports.app.get('/', (req, res) => {
+    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+    res.status(200).json({ version: '1.0' });
+});
+exports.app.use(setting_1.SETTINGS.PATH.BLOGS, blogsRouter_1.blogsRouter);
+exports.app.use(setting_1.SETTINGS.PATH.POSTS, postsRouter_1.postsRouter);
+exports.app.use(setting_1.SETTINGS.PATH.TESTING, testing_1.testingRouter);
